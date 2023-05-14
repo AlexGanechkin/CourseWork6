2ca25214303ae380647d0d9a83a7d8cb52e565b8
AlexGanechkin <terracat80@gmail.com>
2023-05-14 23:57:12 +0400

    Main steps are done

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -1,8 +1,14 @@
 from django.urls import include, path
+from rest_framework_nested import routers
 
-# TODO настройка роутов для модели
+from ads.views import AdViewSet, CommentViewSet
 
+ad_router = routers.SimpleRouter()
+ad_router.register('ads', AdViewSet, basename='ads')
+comments_router = routers.NestedSimpleRouter(ad_router, r'ads', lookup='ad')
+comments_router.register('comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
-
+    path('', include(ad_router.urls)),
+    path('', include(comments_router.urls))
 ]
--- skymarket/ads/views.py
+++ skymarket/ads/views.py
@@ -1,15 +1,58 @@
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import pagination, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+
+from ads.filters import AdFilter
+from ads.models import Ad, Comment
+from ads.serializers import AdSerializer, AdDetailSerializer, CommentSerializer
 
 
 class AdPagination(pagination.PageNumberPagination):
-    pass
+    page_size = 4
 
 
-# TODO view функции. Предлагаем Вам следующую структуру - но Вы всегда можете использовать свою
 class AdViewSet(viewsets.ModelViewSet):
-    pass
+    queryset = Ad.objects.all()
+    pagination_class = AdPagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = AdFilter
+    serializer_class = AdSerializer
+
+    def get_queryset(self):
+        if self.action == 'me':
+            return Ad.objects.filter(author=self.request.user).all()
+        return Ad.objects.all()
+
+    def get_permissions(self):
+        if self.action in ['list']:
+            self.permission_classes = [AllowAny]
+        elif self.action in ['retrieve', 'create', 'me']:
+            self.permission_classes = [IsAuthenticated]
+        else:
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return AdDetailSerializer
+        return AdSerializer
+
+    @action(detail=False, methods=['get'])
+    def me(self, request, *args, **kwargs):
+        return super().list(self, request, *args, **kwargs)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    pass
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
 
+    def get_queryset(self):
+        ad_instance = get_object_or_404(Ad, id=self.kwargs['ad_pk'])
+        return ad_instance.comment_set.all()
+
+    def perform_create(self, serializer):
+        ad_instance = get_object_or_404(Ad, id=self.kwargs['ad_pk'])
+        user = self.request.user
+        serializer.save(author=user, ad=ad_instance)
